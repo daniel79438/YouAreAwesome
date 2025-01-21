b3154997ad4f6588b6d39d4588bf520cab1c9fdf
Daniel Harris <daniel79438@gmail.com>
2025-01-21 20:53:32 +0000

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Daniel Harris on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
