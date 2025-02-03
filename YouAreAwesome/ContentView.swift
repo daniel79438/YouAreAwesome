ebd5d8f0584ee5e15878fbb23a642825767220e0
Daniel Harris <daniel79438@gmail.com>
2025-02-03 18:58:21 +0000

    Added all the Images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         
@@ -28,14 +29,21 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Press Me") {
+            Button("Show Message") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-
-                message = ( message == message1 ? message2 : message1 )
-                imageName = ( imageName == "image0" ? "image1" : "image0" )
                 
+                message = ( message == message1 ? message2 : message1 )
+                imageName = "image" + String(imageNumber)
+                
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                    
+                }
             }
+            
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .foregroundStyle(.white)
